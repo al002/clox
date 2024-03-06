d994b8ca5f44589500dcd4adce17fd0059dbf5de
al002 <al002@disroot.org>
2024-03-06 11:28:42 +0800

    table delete

--- table.c
+++ table.c
@@ -21,11 +21,18 @@ void freeTable(Table *table) {
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
   uint32_t index = key->hash % capacity;
+  Entry* tombstone = NULL;
 
   for (;;) {
     Entry *entry = &entries[index];
 
-    if (entry->key == key || entry->key == NULL) {
+    if (entry->key == NULL) {
+      if (IS_NIL(entry->value)) {
+        return tombstone != NULL ? tombstone : entry;
+      } else {
+        if (tombstone == NULL) tombstone = entry;
+      }
+    } else if (entry->key == key) {
       return entry;
     }
 
@@ -40,6 +47,7 @@ static void adjustCapacity(Table *table, int capacity) {
     entries[i].value = NIL_VAL;
   }
 
+  table->count = 0;
   for (int i = 0; i < table->capacity; i++) {
     Entry *entry = &table->entries[i];
     if (entry->key == NULL)
@@ -48,11 +56,22 @@ static void adjustCapacity(Table *table, int capacity) {
     Entry *dest = findEntry(entries, capacity, entry->key);
     dest->key = entry->key;
     dest->value = entry->value;
+    table->count++;
   }
 
   FREE_ARRAY(Entry, table->entries, table->capacity);
   table->entries = entries;
   table->capacity = capacity;
+}
+
+bool tableGet(Table *table, ObjString *key, Value* value) {
+  if (table->count == 0) return false;
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  *value = entry->value;
+  return true;
 }
 
 bool tableSet(Table *table, ObjString *key, Value value) {
@@ -64,11 +83,33 @@ bool tableSet(Table *table, ObjString *key, Value value) {
   Entry *entry = findEntry(table->entries, table->capacity, key);
 
   bool isNewKey = entry->key == NULL;
-  if (isNewKey)
+  if (isNewKey && IS_NIL(entry->value))
     table->count++;
 
   entry->key = key;
   entry->value = value;
 
   return isNewKey;
+}
+
+bool tableDelete(Table *table, ObjString *key) {
+  if (table->count == 0) return false;
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  entry->key = NULL;
+  entry->value = BOOL_VAL(true);
+
+  return true;
+}
+
+void tableAddAll(Table* from, Table* to) {
+  for (int i = 0; i < from->capacity; i++) {
+    Entry* entry = &from->entries[i];
+
+    if (entry->key != NULL) {
+      tableSet(to, entry->key, entry->value);
+    }
+  }
 }
